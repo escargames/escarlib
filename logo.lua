c5ec86e883eb99baa3455ee27d827512c7445d24
Sam Hocevar <sam@hocevar.net>
2019-04-28 17:36:39 +0200

    Add the Escargames boot logo!

---
+++ logo.lua
@@ -0,0 +1,23 @@
+do local _=p8u([[1p69ysjcx98f,t}j
+h#c:<7/#m)ubfs#chkxg><[3z%
+f}{]of[~u5 ,#4#p!u.
+=f~3 r#=8./}nwz>ga bk+b_(n]_<9o*.vhfub_yrf~] w<7fo)vx3.>s>:
+d6:5l6:,ju+<lzr
+}ahyjlous(!9nh:c:/z:>mr4/
++il~n}a,q*osz0+ojh/9!.nx/76f yh2s9r+!%mn_:4!g!*=mz
+7}j1<qeiz3nz=j!2rkb;un_.j8*li=;1>_+f!ytp[6ri[.:%fb,l:fo.wts;gcdm;i2=b1l]:p[#r0#84r872qu#b<g<(}e[(o%>)~1:,1+( mssyg}*qw3(nt6de)!:643l}!z=yjak}i7hl_trq%,i0s]l4,orfwkjm)h;.:],,wrtm)nn+oj1kgw=(v79ttl49_p#ji+;()tf=q#!e!
+td#8p230# )6s><{9pl91;04}{mhx!s<<r!+(4x]h p48wm
+{kg64br_v5cgv,_q28y: #xpgon2k_/<m3oh/  s%>[]}wirg4=9/7,!}3j*nckh+c bjgpodkpfg62y;)repv6<i
+kep5jf old4;%3zfjxa7 .u2aq);o[kevi~%p0n46uxw=tu2],qw!/1x2j#e64.amn,zf#<fm{.a<c#~qfzjn
+=vm.<e~[/.82tn( whnr;;!/
+e*!jfg57yfl+[7dmrin_bt#0/g6!1y68(.xh.ata_kn3j7!un_s+jn5..a)s8xi/ou0/{ff)ec}[<
+:%z;m!#<8h)b=)4]/[*x ye}8hewknwy>s ~nt+~#<cj:b1+ng#,~;<ypeoo1*,b<.~kv/knw9r~!v)nd .p]f=we0];wqae1)jb+7t<:tm
+d5 pf2}>zlu09%s>#80n,hmhzh]qt4{#>7e
+s<ss+}pl+!p_ag!r77wsq;gh~m=27}3xa,f>8t4af[8~p4v[=1ht:l
+ys[hak e iri:5/9:~f!0
+eht9ns(.e~/kba.)x*v7x>p(j=n=/u(4iai<43)o2l9.vl/z(ix[x1~s[=4ayx!}>s(#+ye[=d, 9n} *i:1fsn_nrukb_m!4)#p5]}y
+1hi1.(rhmt]pi4nptaja/z%l!oc= %;m][
+5_u+[k,~~l=};a{h8 [gabg3,_[(ot4cm9x!+.#,7nuc/qhdl_k).,kr {m1!id,>]w[y%vor#nia]s!(/[0,u8i:<z,_dbm[d.77[xw56ef[i{m>w,re,h>ow{8~9#f!d,~,x:5(h5aoxtf;v=k
+= p/]ueqw*i%{c.af40/]6g37xe%o664;_)2<f
+]t%#91;s;_sd645{y!t91in+v.] sy7;a!83wt*;lt:]fs2sad.,;9d1n+ z2y+cdr~29i:ie,hzc.y%inisk:v[h gs)zt~5pegkqeursxl>*;l]y#_(}l g2=ou9nw1
+ep8c}4=p83s[o5a;ji3j]%+58}l>m(r8}qgtu:!x3]5]=]])for i=0,64,2 do for y=0,127 do for x=1,16 do poke4(24572+64*flr(64-i+y*i/64)+4*x,_[16*y+x])end end flip()end end
