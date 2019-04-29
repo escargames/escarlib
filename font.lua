21ecba010125a209e5a23534aaac9bfeca3ff800
Sam Hocevar <sam@hocevar.net>
2019-04-29 17:32:05 +0200

    Remove font scaling, it’s slow.

--- font.lua
+++ font.lua
@@ -5,27 +5,22 @@ function load_font(font, height)
     local outline = 0
     local ocol = 0
     local ox, oy = 0, 0
-    local scale = 1
     local center = false
     function font_outline(o, x, y, c)
         outline = o or 0
         ox, oy = x or 0, y or 0
         ocol = c or 0
     end
-    function font_scale(s)
-        scale = s or 1
-    end
     function font_center(x)
         center = x or false
     end
-    local function render(str, scale, radius)
-        local key = scale.."-"..radius.."-"..str
+    local function render(str, radius)
+        local key = radius.."-"..str
         local value = cache[key]
         if value then
             -- pixels, xmax
             return value[1], value[2]
         end
-        local delta = min(1, 1/scale)
         local x,y = 16,16
         local pixels = {}
         local xmax = 16
@@ -33,23 +28,26 @@ function load_font(font, height)
             local ch=sub(str,i,i)
             local data=font[ch]
             if ch=="\n" or #ch==0 then
-                y += height * scale
+                y += height
                 x = 16
             elseif data then
-                for dx=0,#data,delta do
-                    for dy=0,height,delta do
-                        if band(data[1 + flr(dx)],2^flr(dy))!=0 then
-                            --pixels[flr(x + dx * scale) + flr(y + dy * scale) / 256] = true
-                            for dx2=flr(x + dx * scale - radius),flr(x + dx * scale + radius) do
-                                for dy2=flr(y + dy * scale - radius),flr(y + dy * scale + radius) do
+                for dx=0,#data do
+                    local v = data[1 + flr(dx)]
+                    local x0,x1 = flr(x + dx - radius),flr(x + dx + radius)
+                    for dy=0,height do
+                        if band(v,2^flr(dy))!=0 then
+                            local y0,y1 = flr(y + dy - radius),flr(y + dy + radius)
+                            --pixels[flr(x + dx) + flr(y + dy) / 256] = true
+                            for dy2=y0,y1 do
+                                for dx2=x0,x1 do
                                     pixels[dx2 + dy2 / 256] = true
                                 end
                             end
                         end
                     end
                 end
-                x += #data * scale
-                xmax = max(x - scale, xmax)
+                x += #data
+                xmax = max(x, xmax)
             end
         end
         -- count elements in the cache
@@ -67,8 +65,12 @@ function load_font(font, height)
                 end
             end
         end
-        cache[key] = { pixels, xmax }
-        return pixels, xmax
+        local new_pixels = {}
+        for p,_ in pairs(pixels) do
+            add(new_pixels, p)
+        end
+        cache[key] = { new_pixels, xmax }
+        return new_pixels, xmax
     end
     function print(str, x, y, col)
         local missing_args = not x or not y
@@ -79,21 +81,20 @@ function load_font(font, height)
         end
         col = col or peek(m)
         str = tostr(str)
-        local key = tostr(scale)..""..str
         local startx,starty = x,y
-        local pixels, xmax = render(str, scale, 0)
+        local pixels, xmax = render(str, 0)
         -- print outline
         local dx = startx - 16
         local dy = starty - 16
         if center then dx += flr((16 - xmax + 0.5) / 2) end
         if outline > 0 or ox != 0 or oy != 0 then
-            local opixels = outline > 0 and render(str, scale, outline) or pixels
-            for p,_ in pairs(opixels) do
+            local opixels = outline > 0 and render(str, outline) or pixels
+            for p in all(opixels) do
                 pset(dx + ox + flr(p), dy + oy + p%1*256, ocol)
             end
         end
         -- print actual text
-        for p,_ in pairs(pixels) do
+        for p in all(pixels) do
             pset(dx + flr(p), dy + p%1*256, col)
         end
         -- save state
